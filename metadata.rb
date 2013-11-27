f09720de069916031eeb5146ee354ee1ac2cd320
Tomasz Weissbek <blazeroot@gmail.com>
2013-11-27 18:43:36 +0100

    Ensure there's zsh and git, clonning repo and setting zsh as shell

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             'dotfiles'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Tomasz Weissbek'
+maintainer_email 'blazeroot@gmail.com'
 license          'All rights reserved'
-description      'Installs/Configures dotfiles'
+description      'Installs/Configures blazeroot\'s dotfiles'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+%w(git zsh).each { |cookbook| depends cookbook }
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,33 @@
 # Cookbook Name:: dotfiles
 # Recipe:: default
 #
-# Copyright (C) 2013 YOUR_NAME
-# 
-# All rights reserved - Do Not Redistribute
+# Copyright (C) 2013 Tomasz Weissbek
 #
+
+include_recipe "git"
+include_recipe "zsh"
+
+user = node[:dotfiles][:user]
+home = user['name'] == 'root' ? '/root' : "/home/#{user['name']}"
+
+git "#{home}/.dotfiles" do
+  repository 'https://github.com/blazeroot/dotfiles.git'
+  reference 'master'
+  action :sync
+  enable_submodules true
+end
+
+#bash "clone blazeroot dotfiles" do
+#  user user['name']
+#  cwd home
+#  code <<-EOH
+#    git clone https://github.com/blazeroot/dotfiles.git .dotfiles
+#    git submodule init
+#    git submodule update
+#  EOH
+#end
+
+user user['name'] do
+  action :modify
+  shell '/bin/zsh'
+end
